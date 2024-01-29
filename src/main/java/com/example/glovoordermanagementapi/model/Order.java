be689b8af7bb6c79ac479d3b70f4a6e6a14aa58f
Kateryna2882 <govoryhina82@gmail.com>
2024-01-29 13:21:32 +0200

    Add methods in classes

--- src/main/java/com/example/glovoordermanagementapi/model/Order.java
+++ src/main/java/com/example/glovoordermanagementapi/model/Order.java
@@ -1,4 +1,46 @@
 package com.example.glovoordermanagementapi.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class Order {
+
+    private int orderId;
+    private List<Product> products;
+
+    public Order(int orderId) {
+        this.orderId = orderId;
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public double calculateTotalPrice() {
+        double totalPrice = 0.0;
+        for (Product product : products) {
+            totalPrice += product.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void displayOrderInfo() {
+        System.out.println("Order ID: " + orderId);
+        System.out.println("Products:");
+        for (Product product : products) {
+            System.out.println("- " + product.getName() + ": $" + product.getPrice());
+        }
+        System.out.println("Total Price: $" + calculateTotalPrice());
+    }
+
+    public void removeProduct(Product product) {
+    }
 }
