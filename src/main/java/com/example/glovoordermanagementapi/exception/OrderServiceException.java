e4c204fc0e335abe1001c73d2bdf61f14dd0aff2
Kateryna2882 <govoryhina82@gmail.com>
2024-01-29 13:56:38 +0200

    Add methods in classes

--- src/main/java/com/example/glovoordermanagementapi/exception/OrderServiceException.java
+++ src/main/java/com/example/glovoordermanagementapi/exception/OrderServiceException.java
@@ -1,4 +1,8 @@
 package com.example.glovoordermanagementapi.exception;
 
-public class OrderServiceException {
+public class OrderServiceException extends RuntimeException {
+    public OrderServiceException(String message) {
+
+        super(message);
+    }
 }
